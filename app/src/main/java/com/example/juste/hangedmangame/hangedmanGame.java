2cfeee112e3d437ffdfb4915a0c93cf5a63ee7ff
Mads Justesen <madsjuste89@gmail.com>
2016-01-06 14:15:57 +0100

    started on hangedman game

---
+++ app/src/main/java/com/example/juste/hangedmangame/hangedmanGame.java
@@ -0,0 +1,13 @@
+package com.example.juste.hangedmangame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class hangedmanGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangedman_game);
+    }
+}
