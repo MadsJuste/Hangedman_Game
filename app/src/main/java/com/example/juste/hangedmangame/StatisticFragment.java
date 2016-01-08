b77635d3440797e0247dfbaadf982769257f082f
STENILO <stenilo@gmail.com>
2016-01-08 15:50:13 +0100

    Statistik frag2

---
+++ app/src/main/java/com/example/juste/hangedmangame/StatisticFragment.java
@@ -0,0 +1,17 @@
+package com.example.juste.hangedmangame;
+
+import android.widget.Button;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Konstantin on 08-01-2016.
+ */
+public class StatisticFragment {
+
+    public Text txtPlay;
+    public Text txtWon;
+    public Text txtLost;
+    public Text txtAvTime;
+
+}
