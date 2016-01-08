28b042a87116da8f0454a53e87ce30027c4180d0
STENILO <stenilo@gmail.com>
2016-01-08 15:14:51 +0100

    Statistik frag

--- app/src/main/java/com/example/juste/hangedmangame/LanguageFragment.java
+++ app/src/main/java/com/example/juste/hangedmangame/LanguageFragment.java
@@ -1,15 +1,37 @@
 package com.example.juste.hangedmangame;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Konstantin on 08-01-2016.
  */
-public class LanguageFragment {
+public class LanguageFragment extends Fragment implements View.OnClickListener{
 
     private Button  btnEn;
     private Button btnDk;
 
-    protected void onCreate (Bundle saveIns )
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.languagefragment, container, false);
+        Log.d("language_fragment", "fragmentet blev vist!");
+        btnDk = (Button) view.findViewById(R.id.btnDk);
+        btnDk.setOnClickListener(this);
+        btnEn = (Button) view.findViewById(R.id.btnEn);
+        btnEn.setOnClickListener(this);
 
+
+        return view;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
