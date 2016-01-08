b239737c4c21de3cd8a628a3a89f4136b3ccce13
STENILO <stenilo@gmail.com>
2016-01-08 11:13:50 +0100

    absrz

---
+++ app/src/main/java/com/example/juste/hangedmangame/LanguageFragment.java
@@ -0,0 +1,15 @@
+package com.example.juste.hangedmangame;
+
+import android.widget.Button;
+
+/**
+ * Created by Konstantin on 08-01-2016.
+ */
+public class LanguageFragment {
+
+    private Button  btnEn;
+    private Button btnDk;
+
+    protected void onCreate (Bundle saveIns )
+
+}
